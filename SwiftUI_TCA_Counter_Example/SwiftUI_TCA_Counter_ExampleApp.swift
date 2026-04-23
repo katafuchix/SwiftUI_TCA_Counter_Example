a6dfc9b9d6a827d8ec9b0cd40592412453caaf52
katafuchix <katafuchix@gmail.com>
2026-04-23 14:12:05 +0900

    Initial Commit

---
+++ SwiftUI_TCA_Counter_Example/SwiftUI_TCA_Counter_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCA_Counter_ExampleApp.swift
+//  SwiftUI_TCA_Counter_Example
+//
+//  Created by cano on 2026/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCA_Counter_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI_TCA_Counter_ExampleTests/SwiftUI_TCA_Counter_ExampleTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUI_TCA_Counter_ExampleTests.swift
+//  SwiftUI_TCA_Counter_ExampleTests
+//
+//  Created by cano on 2026/04/23.
+//
+
+import Testing
+@testable import SwiftUI_TCA_Counter_Example
+
+struct SwiftUI_TCA_Counter_ExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
